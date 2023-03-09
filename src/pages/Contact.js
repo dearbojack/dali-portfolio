7ab7d813077154b170c612bc669d99d3209ee829
Dali Han <ross4han@gmail.com>
2023-03-09 08:44:57 +0000

    home & contact pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contact me</h1>
+      <p>If you want to get in touch with me, you can:</p>
+      <ul>
+        <li>Email me at xxx</li>
+        <li>Call me at xxx</li>
+        <li>Connect with me on LinkedIn</li>
+        <li>Find my github repos here.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Portfolio</h1>
+      <p>This is a simple Portfolio written with react.</p>
+      <p>Use the navbar at the top to navigate through different pages.</p>
+    </div>
+  );
+}
+
+export default Home;
