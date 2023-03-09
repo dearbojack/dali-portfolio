1b21a354fa6a9abbc9e7c1a97734236d7b2154c6
Dali Han <ross4han@gmail.com>
2023-03-09 09:59:18 +0000

    basic styling

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './header.css';
 
 function Header() {
   return (
-    <div>
+    <div className='nav-bar'>
       <Link to="/">
         <h1>Dali Portfolio</h1>
       </Link>
-      <ul>
+      <ul className='nav-menu'>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/projects">Porjects</Link></li>
         <li><Link to="/contact">Contact</Link></li>
